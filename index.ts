ccda9989ff20024926f1581994221c2f451227e0
seanomeara96 <seanom96@gmail.com>
2023-05-17 21:44:49 +0100

    added prompts to the database

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import Replicate from "replicate";
 import dotenv from "dotenv";
-import sqlite from "sqlite3";
-import { prompts } from "./prompts";
 import download from "image-downloader";
 import path from "path";
 import crypto from "crypto";
+import { getDatabaseInstance } from "./db";
 
-const db = new sqlite.Database("main.db");
+const db = getDatabaseInstance();
 
 dotenv.config();
 
@@ -18,7 +17,10 @@ const model =
   "mcai/realistic-vision-v2.0:63ed33a0e79013860417f5a9d45f8a53f0b2f1963bdf1d05259c5cbcf2d70107";
 
 const models = [
-  ["ana-agron", "dark blonde, blue-eyed individual irish girl (Ana de Armas | Diana Agron)"],
+  [
+    "ana-agron",
+    "dark blonde, blue-eyed individual irish girl (Ana de Armas | Diana Agron)",
+  ],
   [
     "alba-biel",
     "dark blonde, blue-eyed individual irish girl (Jessica Alba | Jessica Biel)",
@@ -53,9 +55,17 @@ const models = [
   ],
 ];
 
-const [name, influencerName] = models[0]
+const [name, influencerName] = models[0];
 
 async function main() {
+  const prompts: string[] = (
+    (await new Promise(function (resolve, reject) {
+      db.all("SELECT prompt FROM prompts", (err, rows) =>
+        err ? reject(err) : resolve(rows as { prompt: string }[])
+      );
+    })) as { prompt: string }[]
+  ).map((p) => p.prompt);
+
   for (let i = 0; i < prompts.length; i++) {
     try {
       const prompt = prompts[i].replace("influencer_name", influencerName);
@@ -74,13 +84,13 @@ async function main() {
 
       await new Promise(async function (resolve, reject) {
         try {
-
           const generateRandomString = (length: number): string => {
             const bytes = crypto.randomBytes(Math.ceil(length / 2));
-            return bytes.toString('hex').slice(0, length);
+            return bytes.toString("hex").slice(0, length);
           };
 
-          const filePath = "images" + "/" + name + generateRandomString(5) + ".png"
+          const filePath =
+            "images" + "/" + name + generateRandomString(5) + ".png";
 
           const options = {
             url: replicateImgUrl,
@@ -88,7 +98,7 @@ async function main() {
           };
 
           await download.image(options);
-          
+
           db.run(
             "INSERT INTO  images(url, prompt, name, model, file_path) VALUES(?, ?, ?, ?, ?)",
             [replicateImgUrl, prompt, name, model, filePath],
